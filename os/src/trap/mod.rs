2d9a57d6e0ca719a8dc2599d47b42c64f8d1c785
charming-c <jiam.cai@outlook.com>
2024-07-14 17:10:28 +0800

    lab 2

---
+++ os/src/trap/mod.rs
@@ -0,0 +1,10 @@
+use core::arch::global_asm;
+
+global_asm!(include_str!("trap.S"));
+
+pub fn init() {
+    extern "C" { fn __alltraps(); }
+    unsafe {
+        stvec::write(__alltraps as usize, TrapMode::Direct);
+    }
+}
